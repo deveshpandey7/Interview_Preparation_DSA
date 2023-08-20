ac9f54e64dd3956fc76f04a38f7900a50827999b
Devesh Pandey <deveshpandey607@gmail.com>
2023-08-21 00:31:23 +0530

    Questions added with solutions

---
+++ src/Arrays/MoveZeroestoLast.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class MoveZeroestoLast {
+
+    public static void main(String[] args) {
+
+    }
+    public void moveZeroes(int[] nums) {
+
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != 0)
+                nums[j++]=nums[i];
+        }
+        while(j<nums.length) nums[j++]=0;
+    }
+
+}
